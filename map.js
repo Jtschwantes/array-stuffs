c52827150a5092180ea7d32713aab0f9a8493f90
jtschwantes <jtschwantes@gmail.com>
2019-05-01 11:57:23 -0600

    just missing one now

---
+++ map.js
@@ -0,0 +1,8 @@
+/*
+*  (  What does it do?  ) 1. Applies a function to each element in an array
+* and returns a new array. Better version of forEach for most things.
+*  (  Does it edit it?  ) 2. No.
+*  (What does it return?) 3. A new array.
+*  (How can we use this?) 4. 
+*
+*/
---
+++ reduce.js
@@ -0,0 +1,7 @@
+/*
+*  (  What does it do?  ) 1.
+*  (  Does it edit it?  ) 2.
+*  (What does it return?) 3. 
+*  (How can we use this?) 4. 
+*
+*/
